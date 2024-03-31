e7ac1953b0fb0b1ce915a382afb1d8614994aff9
Brett Tanner <bretttanner171@gmail.com>
2024-03-31 21:54:51 +0900

    Finish arrays/slices

--- sum/sum.go
+++ sum/sum.go
@@ -7,3 +7,23 @@ func Sum(numbers []int) int {
 	}
 	return sum
 }
+
+func SumAll(slicesToSum ...[]int) []int {
+	var results []int
+	for _, slice := range slicesToSum {
+		results = append(results, Sum(slice))
+	}
+	return results
+}
+
+func SumAllTails(slicesToSum ...[]int) []int {
+	var results []int
+	for _, slice := range slicesToSum {
+		if len(slice) == 0 {
+			results = append(results, 0)
+			continue
+		}
+		results = append(results, Sum(slice[1:]))
+	}
+	return results
+}
