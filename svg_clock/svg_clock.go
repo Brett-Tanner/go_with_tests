53cfea671ec405d4c685638d42fdac817b26502e
Brett Tanner <bretttanner171@gmail.com>
2024-06-15 09:54:34 +0900

    Finish secondhand testing

--- svg_clock/svg_clock.go
+++ svg_clock/svg_clock.go
@@ -1,6 +1,8 @@
 package svgclock
 
 import (
+	"fmt"
+	"io"
 	"math"
 	"time"
 )
@@ -15,12 +17,19 @@ type Point struct {
 	Y float64
 }
 
-func SecondHand(t time.Time) Point {
+func SVGWriter(w io.Writer, t time.Time) {
+	io.WriteString(w, svgStart)
+	io.WriteString(w, bezel)
+	secondHand(w, t)
+	io.WriteString(w, svgEnd)
+}
+
+func secondHand(w io.Writer, t time.Time) {
 	p := secondHandPoint(t)
 	p = Point{p.X * SECOND_HAND_LENGTH, p.Y * SECOND_HAND_LENGTH}
 	p = Point{p.X, -p.Y}
 	p = Point{p.X + CENTER, p.Y + CENTER}
-	return p
+	fmt.Fprintf(w, `<line x1="150" y1="150" x2="%.3f" y2="%.3f" style="fill:none;stroke:#000;stroke-width:3px;"/>`, p.X, p.Y)
 }
 
 func secondHandPoint(t time.Time) Point {
@@ -33,3 +42,15 @@ func secondHandPoint(t time.Time) Point {
 func secondsInRadians(t time.Time) float64 {
 	return (math.Pi / (30 / float64(t.Second())))
 }
+
+const svgStart = `<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+<!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
+<svg xmlns="http://www.w3.org/2000/svg"
+     width="100%"
+     height="100%"
+     viewBox="0 0 300 300"
+     version="2.0">`
+
+const bezel = `<circle cx="150" cy="150" r="100" style="fill:#fff;stroke:#000;stroke-width:5px;"/>`
+
+const svgEnd = `</svg>`
--- svg_clock/svg_clock_acceptance_test.go
+++ svg_clock/svg_clock_acceptance_test.go
@@ -1,30 +1,64 @@
 package svgclock
 
 import (
+	"bytes"
+	"encoding/xml"
 	"testing"
 	"time"
 )
 
-func TestSecondHand(t *testing.T) {
+type SVG struct {
+	XMLName xml.Name `xml:"svg"`
+	Xmlns   string   `xml:"xmlns,attr"`
+	Width   string   `xml:"width,attr"`
+	Height  string   `xml:"height,attr"`
+	ViewBox string   `xml:"viewBox,attr"`
+	Version string   `xml:"version,attr"`
+	Circle  Circle   `xml:"circle"`
+	Lines   []Line   `xml:"line"`
+}
+
+type Circle struct {
+	Cx float64 `xml:"cx,attr"`
+	Cy float64 `xml:"cy,attr"`
+	R  float64 `xml:"r,attr"`
+}
+
+type Line struct {
+	X1 float64 `xml:"x1,attr"`
+	Y1 float64 `xml:"y1,attr"`
+	X2 float64 `xml:"x2,attr"`
+	Y2 float64 `xml:"y2,attr"`
+}
+
+func TestSVGWriter(t *testing.T) {
 	cases := []struct {
-		Time  time.Time
-		Point Point
+		Time time.Time
+		Line Line
 	}{
-		{
-			simpleTime(0, 0, 0), Point{X: CENTER, Y: CENTER - 90},
-		},
-		{
-			simpleTime(0, 0, 30), Point{X: CENTER, Y: CENTER + 90},
-		},
+		{simpleTime(1, 0, 0), Line{150, 150, 150, 60}},
+		{simpleTime(0, 0, 30), Line{150, 150, 150, 240}},
 	}
 
-	for _, test := range cases {
-		t.Run(testName(test.Time), func(t *testing.T) {
-			got := SecondHand(test.Time)
+	for _, c := range cases {
+		b := bytes.Buffer{}
+		SVGWriter(&b, c.Time)
 
-			if got != test.Point {
-				t.Errorf("Got %v want %v", got, test.Point)
-			}
-		})
+		svg := SVG{}
+		xml.Unmarshal(b.Bytes(), &svg)
+
+		if !containsLine(c.Line, svg.Lines) {
+			t.Errorf("Expected secondhand line %+v in SVG lines %+v", c.Line, svg.Lines)
+		}
 	}
+}
+
+func containsLine(l Line, lines []Line) bool {
+	for _, line := range lines {
+		if line == l {
+			return true
+		}
+	}
+
+	return false
 }
