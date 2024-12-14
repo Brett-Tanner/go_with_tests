d7584020755c71445843c8494851c73b21ca1490
Brett Tanner <bretttanner171@gmail.com>
2024-12-14 09:41:45 +0900

    Use os.File for db to enable truncation

---
+++ first_app/tape_test.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"io"
+	"testing"
+)
+
+func TestTape_Write(t *testing.T) {
+	file, clean := createTempFile(t, "12345")
+	defer clean()
+
+	want := "abc"
+	tape := &tape{file}
+	tape.Write([]byte(want))
+
+	file.Seek(0, 0)
+	newFileContents, _ := io.ReadAll(file)
+
+	got := string(newFileContents)
+
+	if got != want {
+		t.Errorf("got %s, want %s", got, want)
+	}
+}
