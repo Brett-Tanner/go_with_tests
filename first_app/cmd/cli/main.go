3e0c16198fba662036db2c50cb5a4d9ed32fda49
Brett Tanner <bretttanner171@gmail.com>
2025-01-13 21:07:26 +0900

    Add & export new CLI constructor

--- first_app/cmd/cli/main.go
+++ first_app/cmd/cli/main.go
@@ -2,8 +2,28 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+
+	poker "github.com/Brett-Tanner/go_with_tests/firstapp"
 )
+
+const dbFileName = "game.db.json"
 
 func main() {
 	fmt.Println(("Let's play poker!"))
+	fmt.Println(("Type '{name} wins' to record a win"))
+
+	db, err := os.OpenFile(dbFileName, os.O_RDWR|os.O_CREATE, 0666)
+	if err != nil {
+		log.Fatalf("problem opening %s %v", dbFileName, err)
+	}
+
+	store, err := poker.NewFileSystemPlayerStore(db)
+	if err != nil {
+		log.Fatalf("problem creating player store %v", err)
+	}
+
+	game := poker.NewCLI(store, os.Stdin)
+	game.PlayPoker()
 }
